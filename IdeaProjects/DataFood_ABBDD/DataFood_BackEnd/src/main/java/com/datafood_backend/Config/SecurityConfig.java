0f1858521fbf0654ecebdb01ae553ad1ed032615
Gamaliel <ggamaliel718@gmail.com>
2026-05-07 13:44:57 -0600

    Add files via upload

---
+++ IdeaProjects/DataFood_ABBDD/DataFood_BackEnd/src/main/java/com/datafood_backend/Config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.datafood_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF para que Axios pueda hacer POST
+                .cors(cors -> cors.init(null)) // Activa el soporte de CORS
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Permite todas las rutas mientras desarrollas
+                );
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // El puerto de tu Vite
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
